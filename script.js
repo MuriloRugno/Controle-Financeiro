aac2d1191d8267681fb664f9406fcd7f8f064dad
Murilo Rugno <murilo.hrr_@hotmail.com>
2022-05-23 01:00:59 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const transactionsUl = document.querySelector('#transactions')
+
+const dummyTransactions = [
+    { id: 1, name: 'Bolo de brigadeiro', amount: -20 },
+    { id: 2, name: 'Salário', amount: 300 },
+    { id: 3, name: 'Torta de frango', amount: -10 },
+    { id: 4, name: 'Violão', amount: 150 }
+]
+
+const addTransactionIntoDOM = transaction => {
+    const operator = transaction.amount < 0 ? '-' : '+'
+    const CSSClass = transaction.amount < 0 ? 'minus' : 'plus'
+    const amountWithoutOperator = Math.abs(transaction.amount)
+    const li = document.createElement('li')
+
+    li.classList.add(CSSClass)
+    li.innerHTML = `
+    ${transaction.name} <span>${operator} R$ ${amountWithoutOperator}</span><button class="delete-btn">x</button>
+    `
+    transactionsUl.append(li)
+}
+
+const updateBalenceValues = () => {
+    const transactionAmounts = dummyTransactions.map(transaction => transaction.amount)
+    const total = transactionAmounts.reduce((accumulator, transaction) => accumulator + transaction, 0).toFixed(2)
+    const income = transactionAmounts.filter()
+    console.log(total)
+}
+
+const init = () => {
+    dummyTransactions.forEach(addTransactionIntoDOM)
+    updateBalenceValues()
+}
+
+init()
